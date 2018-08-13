c35770732d800693d746e3ab9da282699c4d33b5
Diogo Dias <diogodias@nasajon.com.br>
2018-08-13 11:35:27 -0300

    aula 2.

---
+++ javascript-programando-na-linguagem-web/introducao-javascript/js/principal.js
@@ -0,0 +1,30 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if (pesoValido && alturaValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
